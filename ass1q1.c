902bfee187706a425f32489e892949a0a28b5bfa
Arnaliya Jaiswal <arnajaiswal26@gmail.com>
2025-08-19 00:42:52 +0530

    Create ass1q1.c

---
+++ ass1q1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+  int num1,num2,sum;
+
+  printf("enter num1:",num1);
+  scanf("%d",&num1);
+
+  printf("enter num2:",num2);
+  scanf("%d",&num2);
+
+  sum=num1+num2;
+  printf("addition of two numbers is:%d",sum);
+  return 0;
+}
