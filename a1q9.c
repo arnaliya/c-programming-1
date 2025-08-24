448319bd829dd1b740e487ba568d61227456df03
Arnaliya Jaiswal <arnajaiswal26@gmail.com>
2025-08-24 22:38:50 +0530

    Create a1q9.c

---
+++ a1q9.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+
+  float rs=0,usd=0;
+
+  printf(" enter rupees:");
+  scanf("%f",&rs);
+
+  usd=rs/48;
+
+
+  printf("rupees to dollars is : %f",usd);
+
+  return 0;
+}
