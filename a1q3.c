7e8a77b16bc79b931497efbb407fb683b4ff9ba7
Arnaliya Jaiswal <arnajaiswal26@gmail.com>
2025-08-24 21:58:35 +0530

    Create a1q3.c

---
+++ a1q3.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+
+int num1,num2,multiply;
+
+  printf(" enter num1:",num1);
+  scanf("%d",&num1);
+
+  printf(" enter num2:",num2);
+  scanf("%d",&num2);
+
+  multiply=num1+num2;
+  printf("multiplication of two numbers is:%d",multiply);
+
+  return 0;
+}
