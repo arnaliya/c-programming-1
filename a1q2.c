528bd1839603cc6084d21f31efb3428aa4c78140
Arnaliya Jaiswal <arnajaiswal26@gmail.com>
2025-08-24 21:58:11 +0530

    Create a1q2.c

---
+++ a1q2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int num1,num2,subtraction;
+
+  printf(" enter num1:",num1);
+  scanf("%d",&num1);
+
+  printf(" enter num2:",num2);
+  scanf("%d",&num2);
+
+  subtraction=num1+num2;
+  printf("subtraction of two numbers is:%d",subtraction);
+
+    return 0;
+}
